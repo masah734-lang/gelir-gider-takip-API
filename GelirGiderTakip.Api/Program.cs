fd58bd02cd862f308c20a774b6d4d4d4fc3a0a2e
elmas asya <masah734@gmail.com>
2026-09-05 00:15:30 +0300

    swagger arayüzü eklendi

--- GelirGiderTakip.Api/Program.cs
+++ GelirGiderTakip.Api/Program.cs
@@ -12,6 +12,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint(
+            "/openapi/v1.json",
+            "Gelir Gider Takip API v1");
+    });
 }
 
 app.UseHttpsRedirection();
